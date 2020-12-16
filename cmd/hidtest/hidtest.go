214b7cf164d9b5e6684604367da0199bcdb16c15
BlinkTheThings <BlinkTheThings@gmail.com>
2020-12-16 09:25:45 -0500

    Start adding hid_enumerate function

--- cmd/hidtest/hidtest.go
+++ cmd/hidtest/hidtest.go
@@ -8,8 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("gohidapi test/example tool. Compiled with hidapi version %s.\n", hidapi.VersionStr())
-
 	major, minor, patch := hidapi.Version()
 	fmt.Printf("gohidapi test/example tool. Compiled with hidapi version %d.%d.%d.\n", major, minor, patch)
 
@@ -19,4 +17,13 @@ func main() {
 	}
 
 	defer hidapi.Exit()
+
+	devices := hidapi.Enumerate(0, 0)
+	for _, dev := range devices {
+		fmt.Printf("Device found\n  type: %04x %04x\n  path: %s\n", dev.VendorID, dev.ProductID, dev.Path)
+		fmt.Printf("  Release:      %x\n", dev.Release)
+		fmt.Printf("  Interface:    %d\n", dev.Interface)
+		fmt.Printf("  Usage (page): 0x%x (0x%x)\n", dev.Usage, dev.UsagePage)
+		fmt.Println()
+	}
 }
--- pkg/hidapi/hidapi.go
+++ pkg/hidapi/hidapi.go
@@ -8,6 +8,16 @@ package hidapi
 */
 import "C"
 
+type DeviceInfo struct {
+	VendorID  uint16
+	ProductID uint16
+	Path      string
+	Release   uint16
+	Interface int
+	Usage     uint16
+	UsagePage uint16
+}
+
 // VersionStr returns the version string of the HIDAPI library.
 func VersionStr() string {
 	return C.GoString(C.hid_version_str())
@@ -28,4 +38,31 @@ func Init() int {
 // Exit finalizes the HIDAPI library.
 func Exit() int {
 	return int(C.hid_exit())
+}
+
+func Enumerate(vid int, pid int) (devices []DeviceInfo) {
+
+	devices = make([]DeviceInfo, 0)
+
+	deviceList := C.hid_enumerate(C.ushort(vid), C.ushort(pid))
+
+	curDev := deviceList
+
+	for curDev != nil {
+		devices = append(devices, DeviceInfo{
+			VendorID:  uint16(curDev.vendor_id),
+			ProductID: uint16(curDev.product_id),
+			Path:      C.GoString(curDev.path),
+			Release:   uint16(curDev.release_number),
+			Interface: int(curDev.interface_number),
+			Usage:     uint16(curDev.usage),
+			UsagePage: uint16(curDev.usage_page),
+		})
+
+		curDev = curDev.next
+	}
+
+	C.hid_free_enumeration(deviceList)
+
+	return
 }
